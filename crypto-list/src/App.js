529f45b7b6fcf8f576f97fd850d8c8c0fd41674c
lc0709 <3dollee@gmail.com>
2024-08-26 18:36:07 +0900

    Feat: simple converter

--- crypto-list/src/App.js
+++ crypto-list/src/App.js
@@ -1,8 +1,57 @@
- 
+ import { useState, useEffect } from "react";
+
 function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [value, setValue] = useState(0);
+  
+  function parsedValue() {
+    let coin = coins[index];
+    let price = coin.quotes.USD.price;
+    let parsedValue = value / price;
+
+    return parsedValue + " " + coin.symbol;
+  }
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  
+  const onSelect = (event) => {
+    let value = event.target.value;
+    setIndex(value);
+  };
+  
+  const onChange = (event) => {
+    let value = event.target.value;
+    setValue(value)
+  }
+
   return (
     <div>
-      <h1>Boilerplate of Clear React Proj.</h1> 
+      <h1>Coins ({coins.length})</h1> 
+      {
+        isLoading 
+          ? <strong>Loading...</strong> 
+          : <select onChange={onSelect}>
+              {
+                coins.map((coin) => (
+                  <option key={coin.id} value={coins.indexOf(coin)}>
+                    {coin.name} ({coin.symbol})
+                  </option>
+                ))
+              }
+            </select>
+      }
+      <input type="text" onChange={onChange} placeholder="type your USD..."/>
+      <hr />
+      {coins.length > index ? <h3>{parsedValue()}</h3> : null}
     </div>
   );
 }
